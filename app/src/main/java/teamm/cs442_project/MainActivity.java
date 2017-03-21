1a4a2b81499b3eed4ac837a8a83c4d89c1ada1f8
Nick Leong <Nick Leong>
2017-03-21 14:49:27 -0400

    location update thread

--- app/src/main/java/teamm/cs442_project/MainActivity.java
+++ app/src/main/java/teamm/cs442_project/MainActivity.java
@@ -15,12 +15,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    GoogleMap g_map;
 
     ImageView profileImgBtn;
     float radius;
@@ -55,6 +60,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         locationListener = new FELocationListener();
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, radius, locationListener);
 
+        final Handler handler = new Handler();
+        final Runnable map_updater = new Runnable() {
+            @Override
+            public void run() {
+                //while(true){
+                    updateMap();
+                    handler.postDelayed(this, 1000);
+                //}
+            }
+        };
+        handler.postDelayed(map_updater, 1000);
     }
 
     @Override
@@ -65,7 +81,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         double currLat = l.getLatitude(); //40.523317;
         double currLon = l.getLongitude(); //-74.44095099999998;
 
+        g_map = map;
+
         map.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(currLat, currLon), 16, 0, 0)));
         map.addMarker(new MarkerOptions().position(new LatLng(currLat, currLon)).title("Marker"));
+    }
+
+    private void updateMap() throws SecurityException{
+        Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        double currLat = l.getLatitude(); //40.523317;
+        double currLon = l.getLongitude(); //-74.44095099999998;
+
+        if(g_map != null){
+            g_map.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(currLat, currLon), 16, 0, 0)));
+            //g_map.addMarker(new MarkerOptions().position(new LatLng(currLat, currLon)).title("Marker"));
+        }
     }
 }
