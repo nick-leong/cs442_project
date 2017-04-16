6e34390dfefb093a9c31fd1eebca607db20390d8
Vignesh Patel <vdp28@scarletmail.rutgers.edu>
2017-04-16 17:53:26 -0400

    more Database and user stuff

--- app/src/main/java/teamm/cs442_project/DatabaseHelper.java
+++ app/src/main/java/teamm/cs442_project/DatabaseHelper.java
@@ -4,5 +4,70 @@ package teamm.cs442_project;
  * Created by Vignesh on 4/16/2017.
  */
 
-public class DatabaseHelper {
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import teamm.cs442_project.user;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    private static final int DATABASE_VERSION = 1;  // database version
+    private static final String DATABASE_NAME = "UserManager.db";   // database name
+    private static final String TABLE_USER = "user";    // user table name
+
+    //user table column names
+    private static final String COLUMN_USER_ID = "user_id";
+    private static final String COLUMN_USERNAME = "user_username";
+    private static final String COLUMN_EMAIL = "user_email";
+    private static final String COLUMN_PASSWORD = "user_password";
+    private static final String COLUMN_CLAN = "user_clan";
+
+
+    // create table sql query
+    private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "("
+            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COLUMN_USERNAME + " TEXT,"
+            + COLUMN_EMAIL + " TEXT,"
+            + COLUMN_PASSWORD + " TEXT,"
+            + COLUMN_CLAN + " TEXT" + ")";
+
+    //drop table sql query
+    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_USER;
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_USER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop user table if exists
+        db.execSQL(DROP_USER_TABLE);
+
+        //create table again
+        onCreate(db);
+    }
+
+    public void addUser(user user){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USERNAME,user.getUserName());
+        values.put(COLUMN_EMAIL,user.getEmail());
+        values.put(COLUMN_PASSWORD,user.getPassword());
+        values.put(COLUMN_CLAN,user.getClan());
+
+        db.insert(TABLE_USER,null,values);
+        db.close();
+
+    }
 }
--- app/src/main/java/teamm/cs442_project/user.java
+++ app/src/main/java/teamm/cs442_project/user.java
@@ -5,4 +5,34 @@ package teamm.cs442_project;
  */
 
 public class user {
+    // Private info
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String clan;
+
+    public user(int id, String userName, String password, String email, String clan){
+        this.id = id;
+        this.username = userName;
+        this.password = password;
+        this.email = email;
+        this.clan = clan;
+    }
+
+    public int getId(){    return id;    }
+    public void setId(int id){   this.id = id;   }
+
+    public String getUserName(){    return username;    }
+    public void setUsername(String username){   this.username = username;   }
+
+    public String getPassword(){    return password;    }
+    public void setPassword(String password){   this.password = password;   }
+
+    public String getEmail(){    return email;    }
+    public void setEmail(String email){   this.password = email;   }
+
+    public String getClan(){    return clan;    }
+    public void setClan(String clan){   this.clan = clan;   }
+
 }
