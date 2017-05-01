ec3f8ad963289f3fe3763086faf38cde696d4429
Vignesh Patel <vdp28@scarletmail.rutgers.edu>
2017-04-30 22:51:44 -0400

    buttons

--- app/src/main/java/teamm/cs442_project/MainActivity.java
+++ app/src/main/java/teamm/cs442_project/MainActivity.java
@@ -29,6 +29,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.content.pm.ApplicationInfo;
 
 import static java.lang.Thread.sleep;
 
@@ -151,4 +154,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         }
     }
+
+    public void showFBPage(View view){
+
+    }
+
+    public static Intent newFacebookIntent(PackageManager pm, String url) {
+        Uri uri = Uri.parse(url);
+        try {
+            ApplicationInfo applicationInfo = pm.getApplicationInfo("com.facebook.katana", 0);
+            if (applicationInfo.enabled) {
+                // http://stackoverflow.com/a/24547437/1048340
+                uri = Uri.parse("fb://facewebmodal/f?href=" + url);
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return new Intent(Intent.ACTION_VIEW, uri);
+    }
+
 }
